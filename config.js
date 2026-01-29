01fdd1e1fa5b8cf8f041f0a6e0599bb9f6a0769b
t-gyoda <tomoya.gyoda@contrea.jp>
2026-01-30 00:24:10 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbyfEQR4AWKvCeePKphfZDKnsMdRGcteNrjymmpwp9Cl1-dLIzyOKUGTK5kfgJo6qnHjng/exec"
+  GAS_URL: "https://script.google.com/macros/s/AKfycbxtHlZXHwi-PcPMIi54NlGsZOAcaM2aps0xUVjbR1vYxuAGSzNJ1yE82dZtSucV8ZCmRw/exec"
 };
 
